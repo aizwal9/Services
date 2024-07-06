17d9bcf0f64400d7b6f263c345d5c23e2b66d350
aizwal <jaiswalaman97@gmail.com>
2024-07-06 21:41:49 +0530

    Added Async call for Notification service

--- customer/src/main/java/com/services/customer/CustomerService.java
+++ customer/src/main/java/com/services/customer/CustomerService.java
@@ -1,5 +1,6 @@
 package com.services.customer;
 
+import com.services.amqp.RabbitMQMessageProducer;
 import com.services.client.fraud.FraudCheckResponse;
 import com.services.client.fraud.FraudClient;
 import com.services.client.notification.NotificationClient;
@@ -14,6 +15,7 @@ public class CustomerService {
     private final CustomerRepository customerRepository;
     private final NotificationClient notificationClient;
     private final FraudClient fraudClient;
+    private final RabbitMQMessageProducer rabbitMQMessageProducer;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -34,7 +36,14 @@ public class CustomerService {
             throw new IllegalStateException("fraudster");
         }
         // todo : make it async (add to queue)
-        notificationClient.sendNotification(new NotificationRequest(customer.getId(),customer.getEmail(),
-                String.format("Hi %s, welcome to microservices tutorial...",customer.getFirstName())));
+        NotificationRequest notificationRequest = new NotificationRequest(customer.getId(), customer.getEmail(),
+                String.format("Hi %s, welcome to microservices tutorial...", customer.getFirstName()));
+//        notificationClient.sendNotification(notificationRequest); Dont need anymore after queue
+
+        rabbitMQMessageProducer.publish(
+                notificationRequest,
+                "internal.exchange",
+                "internal.notification.routing-key"
+        );
     }
 }
