d6f4bf922f5c75c35d9a72214fa4961c0b5c8706
aizwal <jaiswalaman97@gmail.com>
2024-07-13 17:15:09 +0530

    Kubernetes config setup

--- client/src/main/java/com/services/client/notification/NotificationClient.java
+++ client/src/main/java/com/services/client/notification/NotificationClient.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(
-        value = "notification"
+        name = "notification",
+        url = "${client.notification.url}"
 )
 public interface NotificationClient {
     @PostMapping("api/v1/notification")
--- notification/src/main/java/com/services/notification/NotificationApplication.java
+++ notification/src/main/java/com/services/notification/NotificationApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -14,9 +16,12 @@ import org.springframework.context.annotation.Bean;
         }
 )
 @EnableEurekaClient
+@PropertySources({
+        @PropertySource("classpath:client-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NotificationApplication.class,args);
+        SpringApplication.run(NotificationApplication.class, args);
     }
 
 
